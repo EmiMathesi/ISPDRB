43de9c26ed009cb857075af0f07d2189dc7b35d1
MATHESI <emimathesi@mail.ru>
2023-05-16 22:23:40 +0500

    Добавлена страница "Статьи"

--- ISPDRB/main/views.py
+++ ISPDRB/main/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 menu = [
     {'namemenu': 'Главная', 'url_name': 'main'},
-    {'namemenu': 'Статьи', 'url_name': 'main'},
+    {'namemenu': 'Статьи', 'url_name': 'articles'},
     {'namemenu': 'О проекте', 'url_name': 'about'}
 ]
 def index(request):
@@ -20,3 +20,10 @@ def about(request):
         'title': 'О проекте'
     }
     return render(request, 'main/about.html', context=context)
+
+def articles(request):
+    context = {
+        'menu': menu,
+        'title': 'Статьи'
+    }
+    return render(request, 'main/articles.html', context=context)
